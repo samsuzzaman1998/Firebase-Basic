1d57e41cf022b76b345a398fa5fc44a03f56bde3
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-04-19 13:03:27 +0600

    showing signIn info in the UI

--- src/Pages/GoogleSignIn.js
+++ src/Pages/GoogleSignIn.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import FirebaseApp from "../firebase-init";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
@@ -6,17 +6,29 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 const auth = getAuth(FirebaseApp);
 
 const GoogleSignIn = () => {
+    const [userInfo, setUserInfo] = useState({});
     // create a google Provider
     const provider = new GoogleAuthProvider();
 
     const handleGoogleSignInBtn = () => {
         signInWithPopup(auth, provider)
-            .then((res) => console.log(res.user))
+            .then((res) => {
+                const user = res.user;
+                const { displayName, email, photoURL, uid } = user;
+                const userData = { displayName, email, photoURL, uid };
+                setUserInfo(userData);
+                console.log(userData);
+            })
             .catch((err) => console.log(err));
     };
     return (
         <div>
             <button onClick={handleGoogleSignInBtn}>sign in with google</button>
+            <div>
+                <h3>Name: {userInfo.displayName}</h3>
+                <p>Email: {userInfo.email}</p>
+                <img src={userInfo.photoURL} alt="profile" />
+            </div>
         </div>
     );
 };
